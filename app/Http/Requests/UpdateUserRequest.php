f3f393e368eef21d7fcf0038227c4cf1a8d9cfec
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-08-29 15:39:25 +0200

    now the update works also using same email

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class UpdateUserRequest extends FormRequest
 {
     public function rules()
     {
+        // where this $this->user came from? Check this: https://www.csrhymes.com/2019/06/22/using-the-unique-validation-rule-in-laravel-form-request.html
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
+            'email' => ['required', 'string', 'max:255', 'email', Rule::unique('users')->ignore($this->user)],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
     }
