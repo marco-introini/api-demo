634a6f55c385ebfb5e8e01ffe3b6168b0add980d
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-08-29 11:45:37 +0200

    add UserController and api routes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return UserResource::collection(User::paginate(5));
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
+// complete usage of resource
 Route::apiResource('users', UserController::class);
+
+// it's possible to fine tune endpoints in this way:
+Route::prefix('users2')
+    ->name('users2')
+    ->group(function() {
+        Route::get('/',[UserController::class,'index'])->name('index');
+        Route::post('/',[UserController::class,'store'])->name('store');
+        Route::get('/{user}',[UserController::class,'show'])->name('show');
+        Route::put('/{user}',[UserController::class,'update'])->name('update');
+        Route::delete('/',[UserController::class,'delete'])->name('delete');
+    });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
